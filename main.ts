90a8316807a06cd93882bca15f46af5f4d78b37b
Tom Childress <childressiv.dev@gmail.com>
2023-05-28 21:56:40 -0700

    basic files

---
+++ main.ts
@@ -0,0 +1,27 @@
+//memory game 
+/*
+[ ] - 8 pairs of cards show up
+  [ ] - 3 rows, 2 columns 
+  [ ] - cards show numbers
+    [ ] - create variations of each game (picture matching, shape matching, etc.)
+
+[ ] - On Click: flip cards
+
+[ ] - Goal: find all matching pairs
+  [ ] - if all are found -> next level (saves points)
+
+[ ] - Scoring system
+  [ ] - correct match = +1pt
+  [ ] - less time = more points 
+    [ ] - complete under 10 seconds = +3 pts bonus
+[ ] - Reset button
+
+[ ] - difficulty levels 
+  [ ] - add 1 more pair each level 
+  [ ] - level counter 
+
+[ ] - timer 
+
+[ ] - failed 
+
+*/
